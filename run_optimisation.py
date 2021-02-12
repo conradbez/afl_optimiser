b791d4d5b058b4e0e85697fea224fec38fc4923c
Your Name <you@example.com>
2021-02-12 11:28:00 +0800

    update players per position

--- run_optimisation.py
+++ run_optimisation.py
@@ -11,9 +11,6 @@ full_player_scores = full_player_scores[full_player_scores['Year']==2020]
 # Only keep players who are playing this season
 full_player_scores[full_player_scores['Name'].isin(full_player_scores_2021['Name'].unique())].dropna(how='all').dropna(how='all')
 
-
-# full_player_scores = full_player_scores[full_player_scores.isin(full_player_scores_2021['Name'].unique())]['Name'].dropna(how='all')
-
 player_num_games_played = full_player_scores[full_player_scores['Score']>0].groupby('Name')['Team 2'].transform('count')
 
 opponent_average_score = full_player_scores.groupby('Team 2')['Score'].transform('mean')
@@ -24,17 +21,12 @@ na_locs = full_player_scores['Score'].isna()
 
 full_player_scores.loc[na_locs,'Score'] = ((opponent_average_score+player_average_score)/2).loc[na_locs]
 
-# RU_Players = full_player_scores[(full_player_scores['Round']<=2)&(full_player_scores['Year']==2020)] # Only two rounds
-# RU_Players = full_player_scores[(full_player_scores['Round']<=5)&(full_player_scores['Position']=='RU')&(full_player_scores['Year']==2020)] # Only rucks
-# RU_Players = full_player_scores[(full_player_scores['Round']<=2)&(full_player_scores['Year']==2020)]
-# RU_Players = full_player_scores[(full_player_scores['Position']=='RU')&(full_player_scores['Year']==2020)] # Only rucks
 
 RU_Players = full_player_scores[(full_player_scores['Year']==2020)]
 
-
 RU_Players = RU_Players.sort_values('Score',ascending=False)
 RU_Players['id'] = "Round:"+RU_Players['Round'].astype(str)+"_Player:"+RU_Players['Name']
-# RU_Players = RU_Players[RU_Players['Round']<3]
+
 
 # Only take players who played more than 5 games
 print(len(RU_Players['Name'].unique()))
@@ -44,13 +36,17 @@ print(len(RU_Players['Name'].unique()))
 
 
 # Only take players in top quater of average values (score/price)
-# print(len(RU_Players['Name'].unique()))
-# player_best_values = player_average_value.sort_values(ascending=False)[:int(len(player_average_value)/4)]
-# RU_Players = RU_Players.reindex(index=player_best_values.index.values).dropna(how='all') 
+player_best_values = player_average_value.sort_values(ascending=False)[:int(len(player_average_value)/2)]
+RU_Players = RU_Players.reindex(index=player_best_values.index.values).dropna(how='all') 
 print(len(RU_Players['Name'].unique()))
 
+# RU_Players = RU_Players[RU_Players['Round']<4]
+# RU_Players = RU_Players.head(40)
+print(RU_Players.groupby('Position').count()['Name'])
 
-# RU_Players = RU_Players.head(4)
+
+
+
 
 
 trades_allowed = None
@@ -91,13 +87,16 @@ for round, transfers in RU_transfers[RU_transfers['Name_prev'] != RU_transfers['
   prob += lpSum([transfer_contraints[t_id] for t_id in transfers]) <= 4, f"Round: {round}, has less than orequal to 4 transfers"
 # END TRANFERS
 
-# START max players from each position
-allowed_holds_per_position = {'DE': 8, "MI" : 10, 'RU' : 3, 'FO':9}
 
-for (position,round), player in RU_Players[['id','Position','Round']].drop_duplicates().groupby(['Position', 'Round'])['id'].apply(list).iteritems():
-  prob += lpSum([player_contraints[p_id] for p_id in player]) == allowed_holds_per_position[position], f"Position: {position}, has less than {allowed_holds_per_position[position]} in round {round}"
 
-# END max players from each position
+
+# START total players 
+for r, player in RU_Players[['id','Round']].drop_duplicates().groupby(['Round'])['id'].apply(list).iteritems():
+  prob += lpSum([player_contraints[p_id] for p_id in player]) == 30, f"Must have 30 players in round {r}"
+# END total players 
+
+
+
 
 # START money contraint
 money = 13 * 10**6
@@ -105,18 +104,40 @@ for round,player in RU_Players[['id','Price','Round']].drop_duplicates().groupby
   prob += lpSum([player_contraints[p_id]*RU_Players[RU_Players['id']==p_id]['Price'].values[0] for p_id in player]) <= money, f"Round: {round}, has less than ${money}"
 # END money contraint
 
-print('done with pre-work')
-solver = getSolver('COIN_CMD', maxSeconds=2000, msg=True,gapRel = 0.15, threads=100)
+#START Contrain each player to a single position per round
+RU_Players_Position_Casting = RU_Players.copy()
+RU_Players_Position_Casting['pl_pos_id'] = (RU_Players['id']+'_'+RU_Players['Position'])
 
-# solver = getSolver('PULP_CBC_CMD', maxSeconds=100, msg=True,gapRel = 0.1, threads=100)
-# solver = getSolver('COIN_CMD', msg=True, cuts=True)
-# prob.solve(pulp.PULP_CBC_CMD(msg=True, maxSeconds=10))
-# prob.solve(PULP_CBC_CMD(gapRel = 0.05))
-# solver = getSolver('GLPK_CMD')
+Players_Position_Casting_Series = RU_Players_Position_Casting[['id','pl_pos_id','Round']].drop_duplicates().groupby(['Round','id' ])['pl_pos_id'].apply(list).iteritems()
+
+Players_Position_Casting = {}
+for i,r in RU_Players_Position_Casting.iterrows():
+  Players_Position_Casting[r['pl_pos_id']] = LpVariable(r['pl_pos_id'], 0, 1, cat='Binary')
+
+for (round, p_id), pl_pos_ids in Players_Position_Casting_Series:
+    #   Players_Position_Casting[p_id] = [LpVariable(p_id, 0, 1, cat='Binary' for p_id  in pl_pos_id)] 
+    player_possible_positions = [Players_Position_Casting[pl_pos_id] for pl_pos_id  in pl_pos_ids] 
+    # print(p_id)
+    prob += lpSum(player_possible_positions) == player_contraints[p_id], f"Must have only have equal to {p_id} (0 or 1) of {player_possible_positions} positions in round {round}"
+#END Contrain each player to a single position per round
+
+# START max players from each position
+allowed_holds_per_position = {'DE': 8, "MI" : 10, 'RU' : 3, 'FO':9}
+
+for (position,round), pl_pos_ids in RU_Players_Position_Casting[['pl_pos_id','Position','Round']].drop_duplicates().groupby(['Position', 'Round'])['pl_pos_id'].apply(list).iteritems():
+  prob += lpSum([player_contraints[p_id] for pl_pos_id in pl_pos_ids]) >= allowed_holds_per_position[position], f"Position: {position}, has less than {allowed_holds_per_position[position]} in round {round}"
+
+# END max players from each position
+
+
+print('done with pre-work')
+
+
+
+# prob.solve(pulp.PULP_CBC_CMD(msg=True, maxSeconds=200))
+
+solver = getSolver('COIN_CMD', maxSeconds=2000, msg=True,gapRel = 0.15)
 prob.solve(solver)
-# pulp.COIN(maxSeconds=your_time_limit))
-# prob.solve(solver)
-# list_solvers(onlyAvailable=True)
 
 
 roundplayers = {}
@@ -134,4 +155,3 @@ for round in range(1,30):
 
 print(roundchanges)
 print(roundplayers)
-
