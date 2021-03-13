1709b7887c1525aee7e8b73f12111e5a659bffd2
Your Name <test>
2021-03-13 12:48:57 +0800

    change from player specific id to player_position speciic id

--- AFL_optimise.py
+++ AFL_optimise.py
@@ -2,12 +2,20 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 import pandas as pd
+import numpy as np
 from pulp import *
 
 
 # %%
+FILENAME = 'Test_data/test - 1'
+FILENAME = 'Test_data/test - 2'
+FILENAME = 'Test_data/test - 3'
+FILENAME = 'full_players'
+
 full_players = pd.read_csv('full_players.csv', index_col=0).reset_index(drop=True)
-# full_players = full_players[::50]
+
+
+full_players = pd.read_csv(FILENAME+'.csv', index_col=0).reset_index(drop=True)
 
 full_players['Score'] = pd.to_numeric(full_players['Score'].replace('-',0))
 schedule = pd.read_csv('2021_schedule', index_col=0) 
@@ -17,7 +25,7 @@ player_prev = full_players[full_players['Name'].isin(players_2021['Name'].unique
 player_prev = player_prev.groupby(['Name','Team 2']).mean()['Score']
 
 players_schedule_2021 = players_2021.merge(schedule, on=['Team 1'])
-players_schedule_score_2021 = players_schedule_2021.merge(player_prev, on = ['Name','Team 2'])
+players_schedule_score_2021 = players_schedule_2021.merge(player_prev, on = ['Name','Team 2'], how = 'left')
 
 # add bye's
 all_players_list = players_schedule_score_2021[['Name','Price','Position', 'Team 1']].drop_duplicates()
@@ -28,37 +36,39 @@ all_players_list = all_players_list.merge(all_rounds_list, on=['key']).drop('key
 current_players_list = players_schedule_score_2021.copy()
 missing_players = all_players_list.merge(current_players_list, how='outer',indicator=True)
 missing_players = missing_players[missing_players['_merge'] == 'left_only'].drop('_merge',1)
-missing_players['Score'] = 0
+missing_players['Score']=0
 missing_players['Team 2'] = '_BYE'
 
 players_schedule_score_2021 = players_schedule_score_2021.append(missing_players).reset_index(drop=True)
 # end add bye's
 
 P = players_schedule_score_2021
-# P = P[(P['Round']<3)&(P['Round']<14)]
-# P = P[(P['Round']>10)&(P['Round']<14)]
-# players_to_be_kept = P.copy()
-# players_to_be_kept["Value"] = players_to_be_kept['Score']/players_to_be_kept['Price']
-# players_to_be_kept = players_to_be_kept.groupby('Name').mean()[['Score','Value']]
 
-# players_to_keep = []
-# # num_players_to_keep = int(len(players_to_be_kept)*0.2)
-# num_players_to_keep = int(len(players_to_be_kept)*0.5)
-# players_to_keep += list(players_to_be_kept.sort_values('Score', ascending = False).index[:num_players_to_keep])
-# players_to_keep += list(players_to_be_kept.sort_values('Value', ascending = False).index[:num_players_to_keep])
+players_to_be_kept = P.copy()
+players_to_be_kept["Value"] = players_to_be_kept['Score']/players_to_be_kept['Price']
+players_to_be_kept = players_to_be_kept.groupby('Name').mean()[['Score','Value']]
 
-# P = P[P['Name'].isin(players_to_keep)]
+players_to_keep = []
+num_players_to_keep = int(len(players_to_be_kept)*0.2)
+players_to_keep += list(players_to_be_kept.sort_values('Score', ascending = False).index[:num_players_to_keep])
+players_to_keep += list(players_to_be_kept.sort_values('Value', ascending = False).index[:num_players_to_keep])
+
+P = P[P['Name'].isin(players_to_keep)]
+
+P['Score'] = P['Score'].replace(np.nan,0)
+# P = P[P['Round']<=2]
 
 
 # %%
 P['id'] = "R:"+P['Round'].astype(str)+"_P:"+P['Name']
-
+P['pl_pos_id'] = (P['id']+'_'+P['Position'])
+P['id'] = P['pl_pos_id']
 player_contraints = {}
 prob = LpProblem("aflProblem", LpMaximize)
 
 overall_score = LpVariable('OverallScore',0)
-player_contraints = LpVariable.dicts("Player Contraints", P['id'].unique(), 0, 1, cat='Binary')
-prob += lpSum([player_contraints[p_id[1]]*score[1] for p_id,score in zip(P['id'].iteritems(),P['Score'].iteritems())]), "Total score is maximized"
+player_contraints = LpVariable.dicts("Player Contraints", P['pl_pos_id'].unique(), 0, 1, cat='Binary')
+prob += lpSum([player_contraints[p_id[1]]*score[1] for p_id,score in zip(P['pl_pos_id'].iteritems(),P['Score'].iteritems())]), "Total score is maximized"
 
 
 # %%
@@ -89,49 +99,51 @@ for round, transfers in P_transfers[P_transfers['Name_prev'] != P_transfers['Nam
 # %%
 # START money contraint
 money = 13 * 10**6
-for round,player in P[['id','Price','Round']].drop_duplicates().groupby(['Round'])['id'].apply(list).iteritems():
-    prob += lpSum([player_contraints[p_id]*P[P['id']==p_id]['Price'].values[0] for p_id in player]) <= money, f"Round: {round}, has less than ${money}"
+for round,player in P[['pl_pos_id','Price','Round']].drop_duplicates().groupby(['Round'])['pl_pos_id'].apply(list).iteritems():
+    prob += lpSum([player_contraints[p_id]*P[P['pl_pos_id']==p_id]['Price'].values[0] for p_id in player]) <= money, f"Round: {round}, has less than ${money}"
 # # END money contraint
 
 
 # %%
 #START Contrain each player to a single position per round
 P_Position_Casting = P.copy()
-P_Position_Casting['pl_pos_id'] = (P['id']+'_'+P['Position'])
+# P_Position_Casting['pl_pos_id'] = (P['id']+'_'+P['Position'])
 P_Position_Casting = P_Position_Casting[['id','pl_pos_id','Round','Position']].drop_duplicates()
 
-Players_Position_Casting = LpVariable.dicts("Players_Position_Casting", P_Position_Casting['pl_pos_id'], 0, 1, cat='Binary')
+# Players_Position_Casting = LpVariable.dicts("Players_Position_Casting", P_Position_Casting['pl_pos_id'], 0, 1, cat='Binary')
 
-Players_Position_Casting_Series = P_Position_Casting[['id','pl_pos_id','Round']].drop_duplicates().groupby(['id','Round'])['pl_pos_id'].apply(list).iteritems()
+# Players_Position_Casting_Series = P_Position_Casting[['id','pl_pos_id','Round']].drop_duplicates().groupby(['id','Round'])['pl_pos_id'].apply(list).iteritems()
 
-for (p_id,round), pl_pos_ids in Players_Position_Casting_Series:
-    player_possible_positions = [Players_Position_Casting[pl_pos_id] for pl_pos_id  in pl_pos_ids] 
-    prob += lpSum(player_possible_positions) <= player_contraints[p_id], f"Must have only have equal to {p_id} (0 or 1) of {player_possible_positions} positions in round {round}"
+# for (p_id,round), pl_pos_ids in Players_Position_Casting_Series:
+#     player_possible_positions = [Players_Position_Casting[pl_pos_id] for pl_pos_id  in pl_pos_ids] 
+#     prob += lpSum(player_possible_positions) == player_contraints[p_id], f"Must have only have equal to {p_id} (0 or 1) of {player_possible_positions} positions in round {round}"
   #END Contrain each player to a single position per round
 
 
 # %%
 # # START max players from each position
 allowed_holds_per_position = {'DE': 8, "MI" : 10, 'RU' : 3, 'FO':9}
-# allowed_holds_per_position = {'DE': 1, "MI" : 0, 'RU' : 0, 'FO':0}
-for (position,round), pl_pos_ids in P_Position_Casting[['pl_pos_id','Position','Round']].drop_duplicates().groupby(['Position', 'Round'])['pl_pos_id'].apply(list).iteritems():
-    prob += lpSum([Players_Position_Casting[pl_pos_id] for pl_pos_id in pl_pos_ids]) == allowed_holds_per_position[position], f"Position: {position}, has less than {allowed_holds_per_position[position]} in round {round}"
+# allowed_holds_per_position = {'DE': 1, "MI" : 0, 'RU' : 1, 'FO':0}
+for (position,round), pl_pos_ids in P[['pl_pos_id','Position','Round']].drop_duplicates().groupby(['Position', 'Round'])['pl_pos_id'].apply(list).iteritems():
+    prob += lpSum([player_contraints[pl_pos_id] for pl_pos_id in pl_pos_ids]) == allowed_holds_per_position[position], f"Position: {position}, has less than {allowed_holds_per_position[position]} in round {round}"
+    print(f"Position: {position}, has less than {allowed_holds_per_position[position]} in round {round}")
 # END max players from each position
 
 
 # %%
-# solver = getSolver('COIN_CMD', timeLimit=1000, msg=True,gapRel = 0.2)
-# prob.solve(solver)
-
-solver = getSolver('COIN_CMD', msg=True)
+solver = getSolver('COIN_CMD', timeLimit=4000, msg=True,gapRel = 0.2)
 prob.solve(solver)
 
-# solver = getSolver('PULP_CBC_CMD', maxSeconds=1000, msg=True, fracGap = 0.9)
+# solver = getSolver('PULP_CBC_CMD')
 # prob.solve(solver)
+prob.status
 
 
 # %%
-prob.status
+for k,v in player_contraints.items():
+    if v.value():
+        print(k)
+        print(v.value())
 
 
 # %%
@@ -139,10 +151,11 @@ results = []
 for name, player_position in Players_Position_Casting.items():
     if player_position.value() != 0:
         results.append([name,1])
+        # print('here')
 Players_selected = pd.DataFrame(results, columns=['pl_pos_id', 'is_selected'])
 P_Position_Casting = P_Position_Casting.merge(Players_selected, on = ['pl_pos_id'])
-P_Position_Casting.to_csv('solution.csv')
-P_Position_Casting
+P_Position_Casting.to_csv(FILENAME+'_solution.csv')
+P_Position_Casting.sort_values('Round')
 # print(P_Position_Casting.groupby(['Round','Position']).sum()['is_selected'])
 
 
